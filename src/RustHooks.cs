d420c11b3f9cea1257fe0906a64b8d5c04ee43f6
Flutes <runtz.a@mail.com>
2022-07-10 00:15:46 +0200

    Update missing character

--- src/RustHooks.cs
+++ src/RustHooks.cs
@@ -629,7 +629,7 @@ namespace Oxide.Game.Rust
         [HookMethod("OnInstrumentPlayNote")]
         private object OnInstrumentPlayNote(InstrumentTool instrumentTool, int num, int num2, int num3, float num4)
         {
-            return Interface.Oxide.CallDeprecatedHook("OnInstrumentPlayNote", "OnInstrumentPlayNoteInstrumentTool instrumentTool, int num, int num2, int num3, float num4)",
+            return Interface.Oxide.CallDeprecatedHook("OnInstrumentPlayNote", "OnInstrumentPlayNote(InstrumentTool instrumentTool, int num, int num2, int num3, float num4)",
                 new DateTime(2022, 12, 31), weapon, player);
         }
 
